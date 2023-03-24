1ab72357c0c09df28751effa8ef303b79904f501
Davyd Bredykhin <correodemimobil@gmail.com>
2023-03-24 14:10:10 +0100

    v2 framer motion lib, still need create second comp to mobile v without parallax

--- src/sections/PresentationPage/index.jsx
+++ src/sections/PresentationPage/index.jsx
@@ -1,41 +1,85 @@
 import React from 'react';
-import './styles.scss';
 import styles from './PresentationPage.module.scss';
-import { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import PropTypes from 'prop-types';
-import { Parallax, Background } from 'react-parallax';
 import { Title } from '../../components';
-import bg1 from '../../assets/img/Studio.webp';
-import bg3 from '../../assets/img/Studio6.webp';
-import bg2 from '../../assets/img/Studio2.webp';
-import bg4 from '../../assets/img/Studio7.webp';
-import bg5 from '../../assets/img/Studio3.webp';
 import { presentationPageData } from '../../assets';
+import bg from '../../assets/img/firstPageBg1.png';
+
+function handleScrollDirection(direction) {
+  if (direction === 'down') {
+    window.scrollBy({
+      top: window.innerHeight,
+      behavior: 'smooth',
+    });
+  } else {
+    window.scrollBy({
+      top: -window.innerHeight,
+      behavior: 'smooth',
+    });
+  }
+}
+
+const Image = ({ img, title }) => {
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    // offset: ['start', 'end start'],
+    offset: ['start end', 'end end'],
+  });
+  const y = useTransform(scrollYProgress, [0, 1], ['-50%', '0%']);
+  return (
+    <section ref={ref}>
+      <motion.div
+        style={{
+          y,
+          backgroundImage: `url(${img})`,
+        }}
+        className={styles.background}
+      />
+      <div className={styles.content}>
+        {/*<img src={`${id}`} alt="A London skyscraper" />*/}
+      </div>
+      <h2 className={styles.numbers}>
+        <Title text={title} />
+      </h2>
+    </section>
+  );
+};
 
 export const PresentationPage = () => {
+  const onWheelScroll = (event) => {
+    event.preventDefault();
+    const direction = event.deltaY > 0 ? 'down' : 'up';
+    handleScrollDirection(direction);
+  };
+  React.useEffect(() => {
+    document.querySelector('html').style.overflow = 'hidden';
+    window.addEventListener('wheel', onWheelScroll);
+    return () => {
+      document.querySelector('html').style.overflow = 'null';
+      window.removeEventListener('wheel', onWheelScroll);
+    };
+  }, []);
   return (
-    <div className="Test">
-      {presentationPageData.map((i, index) => (
-        <Parallax strength={400} className="Hello" key={i.title}>
-          <Background>
-            <div
-              className={styles.background}
-              style={{
-                backgroundImage: `url(${i.img})`,
-              }}
-            />
-          </Background>
-          <div className={styles.content}>
-            <Title text={i.title} />
-          </div>
-        </Parallax>
+    <>
+      <div
+        className={styles.first}
+        style={{
+          scrollSnapAlign: 'center',
+          minHeight: '100vh',
+          width: '100%',
+          backgroundImage: `url(${bg})`,
+        }}
+      ></div>
+      {presentationPageData.map((obj, index) => (
+        <Image key={index} img={obj.img} title={obj.title} />
       ))}
-    </div>
+    </>
   );
 };
 
 Image.propTypes = {
   img: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  index: PropTypes.number.isRequired,
 };
