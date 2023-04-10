e1f3bba98145fa1dbb9299c51bafaeb2f5ceee8e
Davyd Bredykhin <correodemimobil@gmail.com>
2023-04-10 20:25:55 +0200

    .

--- src/index.js
+++ src/index.js
@@ -11,34 +11,5 @@ import {
 
 import './styles/index.scss';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            index: true,
-            element: <HomePage />,
-          },
-          {
-            path: '/contacts',
-            element: <ContactsPage />,
-          },
-          {
-            path: '/services',
-            element: <ServicesPage />,
-          },
-        ],
-      },
-    ],
-  },
-]);
-
-const App = () => {};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<RouterProvider router={router} />);
